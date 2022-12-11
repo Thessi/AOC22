6f7c7665ed79355c28bdb11b227e8fb01586ebb0
Stefan Straka <stefan.straka@cloudflight.io>
2022-12-11 10:47:01 +0100

    Day 8-2

--- src/main/kotlin/day8/day8.kt
+++ src/main/kotlin/day8/day8.kt
@@ -1,6 +1,7 @@
 package day8
 
 import IOUtil
+import kotlin.math.max
 
 fun main() {
     val inputLines = IOUtil.readResourceFile("input_8.txt")
@@ -9,42 +10,63 @@ fun main() {
         return
     }
 
-    println(countVisibleTrees(inputLines))
+    println(getHighestScenicScore(inputLines))
 }
 
-fun countVisibleTrees(input: List<String>): Int {
-    val visibleTrees = HashSet<String>() // Just save coordinates as "x/y" cause idc
+fun getHighestScenicScore(input: List<String>): Int {
+    var highScore = 0
 
-    val topToBottomMax = Array(input.size) { _ -> -1 }
+    val columnIndexedInput = rotateForest(input)
 
-    val bottomToTopCurrVisible = Array(input.size) { _ -> HashMap<String, Int>() }
     input.forEachIndexed { rowNumber, treeRow ->
-        var leftToRightMax = -1
-
-        var rightToLeftCurrVisible = HashMap<String, Int>()
         treeRow.forEachIndexed { columnNumber, tree ->
             val treeHeight = tree.digitToInt()
-            if (treeHeight > leftToRightMax) {
-                visibleTrees.add("$rowNumber/$columnNumber")
-                leftToRightMax = treeHeight
-            }
+            var score = getViewDistanceLeft(input[rowNumber], columnNumber, treeHeight) // L -> R
+            score *= getViewDistanceRight(input[rowNumber], columnNumber, treeHeight) // R -> L
+            score *= getViewDistanceLeft(columnIndexedInput[columnNumber], rowNumber, treeHeight) // B -> T
+            score *= getViewDistanceRight(columnIndexedInput[columnNumber], rowNumber, treeHeight) // T -> B
 
-            rightToLeftCurrVisible = rightToLeftCurrVisible.filter { (_, height) -> height > treeHeight } as HashMap
-            rightToLeftCurrVisible["$rowNumber/$columnNumber"] = treeHeight
-
-            if (treeHeight > topToBottomMax[columnNumber]) {
-                visibleTrees.add("$rowNumber/$columnNumber")
-                topToBottomMax[columnNumber] = treeHeight
-            }
-
-            bottomToTopCurrVisible[columnNumber] = bottomToTopCurrVisible[columnNumber].filter { (_, height) -> height > treeHeight } as HashMap
-            bottomToTopCurrVisible[columnNumber]["$rowNumber/$columnNumber"] = treeHeight
-
+            highScore = max(score, highScore)
         }
-        visibleTrees.addAll(rightToLeftCurrVisible.keys)
     }
 
-    visibleTrees.addAll(bottomToTopCurrVisible.flatMap { it.keys })
+    return highScore
+}
 
-    return visibleTrees.size
+fun rotateForest(input: List<String>): List<String> {
+    val rotatedInput = Array(input[0].length) { _ -> "" }
+
+    for (treeRow in input) {
+        treeRow.forEachIndexed { columnNumber, tree ->
+            rotatedInput[columnNumber] = rotatedInput[columnNumber] + tree
+        }
+    }
+
+    return rotatedInput.toList()
+}
+
+fun getViewDistanceLeft(treeRow: String, columnNumber: Int, treeHouseHeight: Int): Int {
+    if (columnNumber == 0) {
+        return 0
+    }
+    return getViewDistance(treeRow.substring(0, columnNumber).reversed(), treeHouseHeight)
+}
+
+fun getViewDistanceRight(treeRow: String, columnNumber: Int, treeHouseHeight: Int): Int {
+    if (columnNumber == treeRow.length + 1) {
+        return 0
+    }
+    return getViewDistance(treeRow.substring(columnNumber + 1), treeHouseHeight)
+}
+
+private fun getViewDistance(treeRow: String, treeHouseHeight: Int): Int {
+    var distance = 0
+    for (treeHeight in treeRow) {
+        distance++
+        if (treeHeight.digitToInt() >= treeHouseHeight) {
+            break
+        }
+    }
+    return distance
+
 }
