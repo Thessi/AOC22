7e3f03c361c4712160eca85c608ce0482dc47503
Stefan Straka <stefan.straka@cloudflight.io>
2022-12-19 18:49:13 +0100

    Day 16 - 2

--- src/main/kotlin/day16/day16.kt
+++ src/main/kotlin/day16/day16.kt
@@ -1,13 +1,12 @@
 package day16
 
 import IOUtil
-import java.time.LocalDateTime
 import kotlin.math.ceil
 import kotlin.math.max
 import kotlin.math.min
 
 // 29 since 30 didn't work, off by one is the programmers bane
-const val time: Int = 29
+const val time: Int = 26
 var upperBound: Int = Int.MIN_VALUE
 
 fun main() {
@@ -17,10 +16,8 @@ fun main() {
         return
     }
 
-    println(LocalDateTime.now())
     val rooms = parseInput(inputLines)
     findBestPath(rooms)
-    println(LocalDateTime.now())
     println(upperBound)
 }
 
@@ -28,30 +25,56 @@ private fun findBestPath(rooms: List<CaveRoom>) {
     val sortedValves = rooms.map { Pair(it.valveLabel, it.flowRate) }.sortedByDescending { it.second }
     val startRoom = rooms.find { it.valveLabel == "AA" }!!
 
-    walkThePath(startRoom, sortedValves, time, ArrayList(), 0)
+    walkThePath(startRoom, startRoom, sortedValves, time, ArrayList(), 0)
 }
 
-private fun walkThePath(room: CaveRoom, sortedValves: List<Pair<String, Int>>, minutesRemaining: Int,
+private fun walkThePath(meRoom: CaveRoom, elephantRoom: CaveRoom, sortedValves: List<Pair<String, Int>>, minutesRemaining: Int,
                         turnedOnValves: ArrayList<String>, releasePressure: Int) {
     if (minutesRemaining < 0) {
         upperBound = max(upperBound, releasePressure)
         return
     }
 
-    if (getBestPossibleValue(releasePressure, turnedOnValves, sortedValves, minutesRemaining) < upperBound) {
+    if (getBestPossibleValue(releasePressure, turnedOnValves, sortedValves, minutesRemaining) <= upperBound) {
         return
     }
 
-    room.connectedRooms.forEach {
-        // continue without turning on valve
-        walkThePath(it, sortedValves, minutesRemaining - 1, turnedOnValves, releasePressure)
-
-        // continue with turning on valve
-        if (it.flowRate > 0 && !turnedOnValves.contains(it.valveLabel)) {
+    // turn on both valves
+    if (meRoom.flowRate > 0 && !turnedOnValves.contains(meRoom.valveLabel) &&
+        elephantRoom.flowRate > 0 && !turnedOnValves.contains(elephantRoom.valveLabel) &&
+        meRoom.valveLabel != elephantRoom.valveLabel) {
             val newTurnedOnValves = turnedOnValves.toMutableList() as ArrayList
-            newTurnedOnValves.add(it.valveLabel)
-            walkThePath(it, sortedValves, minutesRemaining - 2, newTurnedOnValves,
-                releasePressure + it.flowRate * (minutesRemaining - 1))
+            newTurnedOnValves.add(meRoom.valveLabel)
+            newTurnedOnValves.add(elephantRoom.valveLabel)
+            walkThePath(meRoom, elephantRoom, sortedValves, minutesRemaining - 1, newTurnedOnValves,
+                releasePressure + (meRoom.flowRate + elephantRoom.flowRate) * (minutesRemaining - 1))
+    }
+
+    // turn on valve in meRoom and move elephant
+    if (meRoom.flowRate > 0 && !turnedOnValves.contains(meRoom.valveLabel)) {
+        elephantRoom.connectedRooms.forEach {
+            val newTurnedOnValves = turnedOnValves.toMutableList() as ArrayList
+            newTurnedOnValves.add(meRoom.valveLabel)
+            walkThePath(meRoom, it, sortedValves, minutesRemaining - 1, newTurnedOnValves,
+                releasePressure + meRoom.flowRate * (minutesRemaining - 1))
+        }
+    }
+
+    // turn on valve in elephantRoom and move me
+    if (elephantRoom.flowRate > 0 && !turnedOnValves.contains(elephantRoom.valveLabel)) {
+        meRoom.connectedRooms.forEach {
+            val newTurnedOnValves = turnedOnValves.toMutableList() as ArrayList
+            newTurnedOnValves.add(elephantRoom.valveLabel)
+            walkThePath(it, elephantRoom, sortedValves, minutesRemaining - 1, newTurnedOnValves,
+                releasePressure + elephantRoom.flowRate * (minutesRemaining - 1))
+        }
+    }
+
+    // move both to next room
+    meRoom.connectedRooms.forEach { nextMeRoom ->
+        elephantRoom.connectedRooms.forEach { nextElephantRoom ->
+            walkThePath(nextMeRoom, nextElephantRoom, sortedValves, minutesRemaining - 1,
+                turnedOnValves, releasePressure)
         }
     }
 }
